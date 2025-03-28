de30958de39a0291bbfc79123cac1cad5ff7b1a2
juan-bl <juannicolasdev@gmail.com>
2025-03-27 22:49:42 -0300

    Aula Excecao / Desafio2 - Controle de Fluxo

---
+++ Desafios/src/DesafioControleFluxo/Main.java
@@ -0,0 +1,34 @@
+package DesafioControleFluxo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.err.println(exception);
+        } finally {
+            terminal.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i < contagem+1; i++) {
+            System.out.printf("Imprimindo o número %d%n", i);
+        }
+    }
+}
